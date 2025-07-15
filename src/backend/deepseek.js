73750bb5683bd8b27e6e719362ca4d3c5dca247c
DenkuKuro <javier.deng17@gmail.com>
2025-07-14 17:13:49 -0700

    Tested all file types parsing and increased token for the deepseek api call function

--- src/backend/deepseek.js
+++ src/backend/deepseek.js
@@ -15,7 +15,7 @@ async function queryDeepSeekV3(prompt) {
         model: "deepseek/deepseek-chat-v3-0324:free",
         messages: [{ role: "user", content: prompt }],
         // limits how long the response can be (WE CAN CHANGE THIS)
-        max_tokens: 300,
+        max_tokens: 1000,
         // controls randomness (WE CAN CHANGE THIS)
         temperature: 0.7,
       }),
@@ -24,6 +24,7 @@ async function queryDeepSeekV3(prompt) {
     const data = await response.json();
     if(data.choices && data.choices[0]){
       console.log("Raw API response data: ", data.choices[0].message.content);
+      console.log(JSON.parse(data.choices[0].message.content));
     }
     else{
       console.error("Unexpected/empty response: ", data);
