bfe16eaca4a648c7ce1210f1f694d525769b9a52
Shelby Haines <srh11@sfu.ca>
2025-07-16 14:50:03 -0700

    Add initial setup for frontend
    Add new folders in our project-src folder to help organize our frontend. This includes a new folder to hold each page, and a new folder to hold components like the header/footer. Added the files for the different pages. Uploaded an image to the 'assets' folder that contains the logo for our website. Set up the App.jsx file to hold each page of our website, as well as our header/footer.

---
+++ src/frontend/project-src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CloudUpload, Clipboard, ChevronRight } from 'lucide-react';
+
+
+function App() {
+  return (
+   <div>
+    <p>this is page 1</p>
+   </div>
+  );
+}
+
+export default App;
+
