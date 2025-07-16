9cbb976a33b01f740d90b21514708db7a8ae7eea
Shelby Haines <srh11@sfu.ca>
2025-07-16 16:10:11 -0700

    Add header component
    Updated Header.jsx to include the logo and website title. The component is exportable and can be reused.

--- src/frontend/project-src/components/Header.jsx
+++ src/frontend/project-src/components/Header.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+// imports the website logo from our assets folder
+import websiteLogo from '../assets/websitelogo.png';
 
+// our main header component that includes the website name and logo
+function Header() {
+ return (
+   <header className="bg-white px-8 py-4 text-center mt-auto">
+     <div className="flex items-center space-x-3">
+        {/* displays the website logo on the top left of the header */}
+       <img src={websiteLogo} alt="Website Logo" className="h-8 w-auto" />
+
+       {/* displays the website name next to the logo */}
+       <h1 className="font-semibold text-2xl text-blue-600">
+         EasyChart
+       </h1>
+     </div>
+   </header>
+ );
+}
+
+
+export default Header;
