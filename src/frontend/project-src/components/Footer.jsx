7a4dbbe16032e692f8d2cddb6d727d955b6dbb04
Shelby Haines <srh11@sfu.ca>
2025-07-16 16:17:28 -0700

    Add footer component
    Update Footer.jsx to include the website name and copyright information. Can be exported and reused.

---
+++ src/frontend/project-src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// resuable footer component 
+function Footer() {
+
+return (
+  <footer className="bg-white text-gray-800 p-4 text-center text-sm mt-auto">
+    <div>
+        {/* displays the copyright symbol, next to the year and website name */}
+      &copy; {2025} EasyChart.
+    </div>
+  </footer>
+);
+}
+
+export default Footer;
