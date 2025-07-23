af2d27177ff85aa105edd0beecae2dae6fc77eb7
Tony Wu <tjw11@sfu.ca>
2025-07-23 11:46:31 -0700

    Modified format of the download buttons, minor adjustment

---
+++ src/frontend/project-src/components/editsave/DownloadButtons.jsx
@@ -0,0 +1,12 @@
+
+function DownloadButtons() {
+    return (
+        <div className="flex gap-4">
+            <button className="format-button">PNG</button>
+            <button className="format-button">PDF</button>
+            <button className="format-button">JPEG</button>
+        </div>
+    );
+}
+
+export default DownloadButtons;
