f9186861643c0d4705376a59a8100b60887a340e
niickzl <20051024nick@gmail.com>
2025-07-25 01:47:20 -0700

    Attempt to fix workload

--- src/backend/__tests__/feature1Integration.test.js
+++ src/backend/__tests__/feature1Integration.test.js
@@ -12,11 +12,11 @@ jest.mock('../feature1.js', () => ({
 
 describe('Integration test of file upload flow', () => {
     test('uploads file and hits backend endpoint', async () => {
-        const filePath = path.join(__dirname, 'files/csv/simple_sample.csv');
+        const filePath = path.resolve(__dirname, 'files/csv/simple_sample.csv');
 
         const res = await request(app)
             .post('/file-submit')
-            .attach('files', filePath);
+            .attach('uploads', filePath);
 
         expect(res.status).toBe(200);
         expect(res.body).toEqual({ message: 'Mocked feature handler called' });
