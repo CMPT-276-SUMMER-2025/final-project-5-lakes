539f518a4f6be556f2a06f91c19539e8fee5f48c
niickzl <20051024nick@gmail.com>
2025-07-25 14:17:55 -0700

    Add comments & fix yml for main only

--- src/backend/__tests__/feature1Integration.test.js
+++ src/backend/__tests__/feature1Integration.test.js
@@ -6,12 +6,14 @@ const fs = require('fs');
 const tempDir = path.resolve(__dirname, 'temp');
 const tempCsvPath = path.join(tempDir, 'temp_sample.csv');
 
+// Create dummy dir and file
 beforeAll(() => {
     //create temp dir
     fs.mkdirSync(tempDir);
     fs.writeFileSync(tempCsvPath, "");
 });
 
+// Delete dummy dir and file
 afterAll(() => {
     if (fs.existsSync(tempCsvPath)) {
         fs.unlinkSync(tempCsvPath);
@@ -32,6 +34,7 @@ jest.mock('../feature1.js', () => ({
 describe('Integration test of file upload flow', () => {
     test('uploads file and hits backend endpoint', async () => {
 
+        //Get response from app
         const res = await request(app)
             .post('/file-submit')
             .attach('files', tempCsvPath);
