c53829974fe87fbdf9831e4995241fca117547ef
niickzl <20051024nick@gmail.com>
2025-07-25 13:29:53 -0700

    Upload test files to github repo

--- src/backend/__tests__/feature1Integration.test.js
+++ src/backend/__tests__/feature1Integration.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app.js');
+const path = require('path');
 const fs = require('fs');
 
 // INTEGRATION TEST 
@@ -12,7 +13,8 @@ jest.mock('../feature1.js', () => ({
 
 describe('Integration test of file upload flow', () => {
     test('uploads file and hits backend endpoint', async () => {
-        const filePath = '/home/runner/work/final-project-5-lakes/src/backend/__tests__/files/csv/simple_sample.csv';
+        const filePath = path.resolve(__dirname, 'files/csv/simple_sample.csv');
+
         console.log('File exists:', fs.existsSync(filePath), 'Path:', filePath);
 
         const res = await request(app)
