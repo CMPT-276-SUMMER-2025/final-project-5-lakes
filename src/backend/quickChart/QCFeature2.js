9aad6ad321ff2b753ceb904080c9a4cddb4fb5f6
niickzl <20051024nick@gmail.com>
2025-07-28 16:54:02 -0700

    Write basic comments on each features to get started

---
+++ src/backend/quickChart/QCFeature2.js
@@ -0,0 +1,5 @@
+// Feature 2: Customize Chart
+// Get returned back the previous data: https://quickchart.io/chart?c=<encoded JSON config>
+// Make an object storing all possible changes
+// Compare the new object with user changes with the fixed parameters as an object
+// Any differences will be applied to the new graph and displayed
