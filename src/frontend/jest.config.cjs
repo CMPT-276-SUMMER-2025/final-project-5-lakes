ddf8ebce0b3055db4d833c11f328d9606ed1da79
DenkuKuro <javier.deng17@gmail.com>
2025-08-07 11:41:56 -0700

    Created unit test for quickchart feature 3

--- src/frontend/jest.config.cjs
+++ src/frontend/jest.config.cjs
@@ -1,4 +1,22 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testMatch: [
+    '<rootDir>/__tests__/**/*.test.(js|jsx|ts|tsx)',
+  ],
+  testEnvironmentOptions: {
+    html: '<html><body></body></html>',
+    url: 'http://localhost',
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
